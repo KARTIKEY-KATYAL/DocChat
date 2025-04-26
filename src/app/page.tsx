2027e56ee897fa7ef53f7136a6bf249faf15d8ad
Kartikey Katyal <kartikeykatyal2003@gmail.com>
2025-04-26 11:09:21 +0530

    clerk auth set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
   <div>
-
+    <h1 className="text-red-600">Hello World!</h1>
+    <Button>Hello To All</Button>
   </div>
   )}
