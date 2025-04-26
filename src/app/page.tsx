d05e6df3da3ef20bf9a9b9161177d44fb07baaed
Kartikey Katyal <kartikeykatyal2003@gmail.com>
2025-04-26 11:37:13 +0530

    home page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,44 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { ArrowRight, LogIn } from "lucide-react";
+import FileUpload from "@/components/FileUpload";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const {userid} = await auth()
+  const isAuth = !!userid
   return (
-  <div>
-    <h1 className="text-red-600">Hello World!</h1>
-    <Button>Hello To All</Button>
-  </div>
-  )}
+    <div className="min-h-screen bg-gradient-to-r from-[#0f172a]  to-[#334155]">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold text-slate-100 text-shadow-amber-500 text-shadow-2xs">
+              Chat With Any PDF
+            </h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+          <p className="max-w-xl mt-1 text-lg text-slate-50">
+            Join millions of students, researchers and professionals to
+            instantly answer questions and understand research with AI
+          </p>
+
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <FileUpload />
+            ) : (
+              <Link href="/sign-in">
+                <Button className="cursor-pointer">
+                  Login to get Started!
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );}
